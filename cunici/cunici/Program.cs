22b4f21c69c096e9cbf09187aba59db3d0692a85
terp1s <piskova.ter@gmail.com>
2022-12-01 19:03:50 +0100

    uz

--- cunici/cunici/Program.cs
+++ cunici/cunici/Program.cs
@@ -1,206 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using NPOI.SS.Formula.Functions;
+using System;
 
 namespace serifove_cunici
 {
-    class Spojak
-    {
-        public Vertex Zacatek { get; set; }
-
-        public Vertex Konec { get; set; }
-
-        public Spojak()
-        {
-
-        }
-    }
-    class Fronta
-    {
-        Spojak Spojak { get; } = new Spojak();
-        public void Vloz(Vertex novy)
-        {
-            
-            if (Spojak.Zacatek == null)
-            {
-                Spojak.Zacatek = novy;
-                Spojak.Konec = Spojak.Zacatek;
-            }
-            else
-            {
-                Spojak.Konec.Next = novy;
-                Spojak.Konec = Spojak.Konec.Next;
-            }
-
-        }
-
-        public int VratPrvek()
-        {
-            return Spojak.Zacatek.Nazev;
-        }
-
-        public void OdeberPrvek()
-        {
-            Spojak.Zacatek = Spojak.Zacatek.Next;
-        }
-
-        public bool Prazdna()
-        {
-            if (Spojak.Zacatek == null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    class Zasobnik
-    {
-        Spojak Spojak { get; } = new Spojak();
-
-        public void Vloz(int i)
-        {
-            Vertex novy = new Vertex();
-            novy.Next = Spojak.Zacatek;
-            Spojak.Zacatek = novy;
-        }
-
-        public int VratPrvek()
-        {
-            return Spojak.Zacatek.Nazev;
-        }
-
-        public void OdeberPrvek()
-        {
-            Spojak.Zacatek = Spojak.Zacatek.Next;
-        }
-
-        public bool Prazdna()
-        {
-            if (Spojak.Zacatek == null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    class Vertex
-    {
-        public List<Vertex> Sousedi = new List<Vertex>();
-        public Vertex Next { get; set; }
-        public int Hloubka;
-        public int Nazev;
-        //public Zasobnik Depth { get; set; }
-        //public Fronta Breadth { get; set; }
-    }
-    class Graf
-    {
-        public List<Vertex> Vrcholy { get; set; }
-
-        public Spojak Cesta { get; set; }
-        public void Pocet_vrcholu(int i)
-        {
-            Vrcholy = new List<Vertex>();
-
-            for (int a = 0; a < i; a++)
-            {
-
-                Vrcholy.Add(new Vertex());
-                Vrcholy[a].Nazev = a;
-            }
-        }
-
-        public void Sousedi(string dvojice)
-        {
-            string[] nazvy = dvojice.Split();
-
-            Vrcholy[int.Parse(nazvy[0])].Sousedi.Add(Vrcholy[int.Parse(nazvy[1])]);
-            Vrcholy[int.Parse(nazvy[1])].Sousedi.Add(Vrcholy[int.Parse(nazvy[0])]);
-        }
-        public void Nacti()
-        {
-            Pocet_vrcholu(int.Parse(Console.ReadLine()));
-            int hrany = int.Parse(Console.ReadLine());
-            for (int i = 0; i < hrany; i++)
-            {
-                Sousedi(Console.ReadLine());
-            }
-            string[] input = Console.ReadLine().Split();
-            Nejkratsi_cesta(Vrcholy[int.Parse(input[0])], Vrcholy[int.Parse(input[1])]);
-
-        }
-
-        public void Nejkratsi_cesta(Vertex start, Vertex cil)
-        {
-            Fronta cesta = new Fronta();
-            List<Vertex> cesticka = new List<Vertex>();
-            List<int> baltik = new List<int>();
-            Vertex policko = start;
-            policko.Hloubka = 0;
-            cesta.Vloz(policko);
-            baltik.Add(policko.Nazev);
-            while (policko != cil)
-            {
-                for (int i = 0; i < policko.Sousedi.Count; i++)
-                {
-                    if(baltik.Contains(policko.Sousedi[i].Nazev) == false)
-                    {
-                        cesta.Vloz(policko.Sousedi[i]);
-                        baltik.Add(policko.Sousedi[i].Nazev);
-                        policko.Sousedi[i].Hloubka = policko.Hloubka + 1;
-                    }
-                    
-                }
-                policko = policko.Next;               
-            }
-
-            int a = 0;
-            cesticka.Add(policko);
-
-            while(policko != start)
-            {
-                
-                if (policko.Sousedi[a].Hloubka != -1 && policko.Sousedi[a].Hloubka < policko.Hloubka)
-                {
-                    policko = policko.Sousedi[a];
-                    cesticka.Add(policko);
-                    a = 0;
-                }
-                else
-                {
-                    a++;
-                }
-            }
-
-            cesticka.Reverse();
-
-            foreach (Vertex item in cesticka)
-            {
-                Console.Write(item.Nazev + " ");
-            }
-
-            foreach(Vertex item in Vrcholy)
-            {
-                item.Hloubka = -1;
-            }
-
-        }
-        
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Graf test = new Graf();
-            test.Nacti();
+            (int,int)[] hrany =
+            {
+                (0, 1),
+                (1, 2),
+                (3, 2),
+                (4, 2),
+                (0,1),
+            };
+
+            
+            GraphDef def = new GraphDef();
+            Graph test = def.Graph;
+
+            def.Pocet_vrcholu(5);
+            def.Pocet_hran(4);
+            def.Sousedi(hrany);
+
+            Vertex start = test.Vrcholy[0];
+            Vertex cil = test.Vrcholy[4];
+
 
             foreach (var item in test.Vrcholy)
             {
-                Console.WriteLine();
+                
                 Console.Write(item.Nazev + ":");
                 
                 foreach (var item2 in item.Sousedi)
@@ -208,8 +38,10 @@ namespace serifove_cunici
                     Console.Write(item2.Nazev + " ");
                     
                 }
+                Console.WriteLine();
             }
-            
+
+            Console.WriteLine(def.Cesta(start, cil));
             
             Console.ReadKey();
         }
